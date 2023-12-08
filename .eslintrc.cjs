f83f2a3ba2e1644a8b85440b5cf4d35bbd533632
Elias Gallo <elias.gallo.90@gmail.com>
2023-12-08 09:32:12 +0100

    fix: lint only staged files

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-// https://typescript-eslint.io/getting-started/
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  env: { node: true },
-};
