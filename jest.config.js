c2955246acf7ea67ab84e8fe0030b7c741a86a6f
Elias Gallo <elias.gallo.90@gmail.com>
2023-12-08 16:24:02 +0100

    test: add jest tests to the project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@shared/(.*)$': '<rootDir>/src/components/shared/$1',
+    '@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '@images/(.*)$': '<rootDir>/src/images/$1',
+    '@store/': '<rootDir>/src/store',
+    '@types/': '<rootDir>/src/types',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
+  }
+};
