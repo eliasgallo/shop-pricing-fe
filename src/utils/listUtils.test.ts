e9e40f32acd7fa61db77a0a98f96a4642c110b8f
Elias Gallo <elias.gallo.90@gmail.com>
2023-12-11 14:13:01 +0100

    minor: remove unused util code

--- src/utils/listUtils.test.ts
+++ src/utils/listUtils.test.ts
@@ -1,10 +1,5 @@
 import { describe, expect, it } from '@jest/globals'
-import {
-  findWithId,
-  concatDistinct,
-  keyList,
-  replaceItemInKeyList,
-} from './listUtils'
+import { findWithId } from './listUtils'
 
 type TestType = {
   id?: number
@@ -26,56 +21,5 @@ describe('findWithId', () => {
 
   it('returns undefined with an unkown id', () => {
     expect(findWithId(list, 0) || findWithId(list, 3)).toBeUndefined()
-  })
-})
-
-describe('concatDistinct', () => {
-  const listA = [1, 2, 3, 4, 5]
-  const listB = [4, 5, 6, 7, 8]
-  it('puts lists together and make result list unique', () => {
-    expect(concatDistinct(listA, listB)).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
-    expect(concatDistinct([], listB)).toEqual(listB)
-  })
-})
-
-describe('keyList', () => {
-  it('changes list to a key-value list with wanted key', () => {
-    expect(keyList(list, 'ok')).toEqual({
-      true: [
-        { name: 'a', ok: true },
-        { id: 1, name: 'b', ok: true },
-      ],
-      false: [
-        { id: 2, name: 'c', ok: false },
-        { name: 'd', ok: false },
-      ],
-    })
-  })
-})
-
-describe('replaceItemInKeyList', () => {
-  const keyList = {
-    a: [
-      { name: 'a', ok: true },
-      { id: 1, name: 'a', ok: true },
-    ],
-    b: [
-      { id: 2, name: 'b', ok: false },
-      { name: 'b', ok: false },
-    ],
-  }
-
-  it('replaces old item properties with new item', () => {
-    const newItem = { ...keyList['b'][0], ok: true }
-    const result = replaceItemInKeyList(keyList, 'b', newItem, 'b', () => 0)
-    expect(result['b']).toEqual([keyList['b'][1], newItem])
-  })
-
-  it('new item moves to different key/list', () => {
-    const newItem = { id: 1, name: 'b', ok: false }
-    const result = replaceItemInKeyList(keyList, 'a', newItem, 'b', () => 0)
-    expect(result['a']).toEqual([keyList.a[0]])
-    expect(result['b']).toHaveLength(3)
-    expect(result['b']).toContain(newItem)
   })
 })
--- src/utils/listUtils.ts
+++ src/utils/listUtils.ts
@@ -3,48 +3,8 @@ export const findWithId = <T extends { id?: number }>(
   findId: number
 ): T | undefined => list.find((e) => e.id === findId)
 
-export const concatDistinct = <T>(arr1: Array<T>, arr2: Array<T>): Array<T> => {
-  return arr1
-    .concat(arr2)
-    .filter((value, index, array) => array.indexOf(value) === index)
-}
-
 export const distinct = <T>(list: T[]) =>
   list.filter((value, index, array) => array.indexOf(value) === index)
-
-type ReturnList<T> = { [key: string]: T[] }
-export const keyList = <Type, Key extends keyof Type>(
-  list: Type[],
-  key: Key
-): ReturnList<Type> => {
-  const result: ReturnList<Type> = {}
-  list.forEach((item: Type) => {
-    const store = (item[key] as keyof string).toString()
-    result[store] = (result[store] || []).concat([item])
-  })
-  return result
-}
-
-export const replaceItemInKeyList = <T extends { id?: number }>(
-  keyList: { [key: string]: T[] },
-  oldItemKey: string | undefined,
-  newItem: T,
-  newItemKey: string,
-  sorting: (a: T, b: T) => number
-): { [key: string]: T[] } => {
-  const listAfterRemoval = oldItemKey
-    ? {
-        ...keyList,
-        [oldItemKey]: sliceItemId(keyList[oldItemKey], newItem.id!),
-      }
-    : { ...keyList }
-
-  const newPriceList = (listAfterRemoval[newItemKey] || [])
-    .concat([newItem])
-    .sort(sorting)
-
-  return { ...listAfterRemoval, [newItemKey]: newPriceList }
-}
 
 export const sliceItemId = <T extends { id?: number }>(
   list: T[],
