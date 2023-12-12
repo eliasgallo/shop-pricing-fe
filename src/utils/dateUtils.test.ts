6c1dd87203f0b4eb78f52ef35063c2857962473a
Elias Gallo <elias.gallo.90@gmail.com>
2023-12-12 14:30:21 +0100

    minor: remove unused util 'dateMinutesOffset'

--- src/utils/dateUtils.test.ts
+++ src/utils/dateUtils.test.ts
@@ -1,23 +1,9 @@
 import { describe, expect, it } from '@jest/globals' // https://jestjs.io/docs/getting-started#type-definitions
-import { dateInFuture, dateMinutesOffset } from './dateUtils'
+import { dateInFuture } from './dateUtils'
 
 describe('dateInFuture', () => {
   it('date should be in past', () => {
     expect(dateInFuture('2022-01-01 11:11:11')).toBe(false)
     expect(dateInFuture(new Date().toISOString())).toBe(false)
-  })
-})
-
-describe('dateMinutesOffset', () => {
-  it('result should be in the future', () => {
-    const currentDate = new Date('2022-01-01 11:11:11')
-    const result = dateMinutesOffset(currentDate, 3)
-    expect(new Date(result).getMinutes()).toBe(14)
-  })
-
-  it('result should be in past', () => {
-    const currentDate = new Date('2022-01-01 11:11:11')
-    const result = dateMinutesOffset(currentDate, -3)
-    expect(new Date(result).getMinutes()).toBe(8)
   })
 })
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,5 +1,2 @@
 export const dateInFuture = (first: string): boolean =>
   Date.parse(first).valueOf() - Date.now() > 0
-
-export const dateMinutesOffset = (date: Date, minutes: number): string =>
-  new Date(date.getTime() + minutes * 60 * 1000).toISOString()
