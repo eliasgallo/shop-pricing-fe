14b607dcbdeb5c5cc3933275b336f28145d6b33a
Elias Gallo <elias.gallo.90@gmail.com>
2023-12-12 14:30:50 +0100

    ref: remove unused hook

--- src/hooks/useActions.ts
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as actionCreators from '../store/action-creators'
-
-export const useActions = () => {
-  const dispatch = useDispatch()
-
-  return bindActionCreators(actionCreators, dispatch)
-}
