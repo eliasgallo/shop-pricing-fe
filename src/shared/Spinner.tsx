c4b1ec04601735927c56e53152c721f69d40d034
Elias Gallo <elias.gallo.90@gmail.com>
2023-12-22 14:05:57 +0100

    fix: center the spinner

--- src/shared/Spinner.tsx
+++ src/shared/Spinner.tsx
@@ -1,25 +1,23 @@
 import styled, { keyframes } from 'styled-components'
 
 const SpinnerKeyframe = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+  0% { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
 `
 
 const SpinnerView = styled.div`
+  box-sizing: border-box;
   border: 5px solid #f3f3f3;
   border-top: 5px solid #3498db;
   border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  animation: ${SpinnerKeyframe} 1s linear infinite;
-
   position: absolute;
   top: 40%;
-  right: 50%;
+  animation: ${SpinnerKeyframe} 1s linear infinite;
+
+  --diam: 40px;
+  width: var(--diam);
+  height: var(--diam);
+  right: calc(50% - var(--diam) / 2);
 `
 
 const SpinnerWrapper = styled.div`
